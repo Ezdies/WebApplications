c30c577ad5448a419d88ecad4db60d170e6cb97d
Ezdies <bvbclubbvb@gmail.com>
2024-02-29 14:02:59 +0100

    Added ex4

---
+++ Lab1/ex4.py
@@ -0,0 +1,16 @@
+import sys
+import socket
+
+def get_hostname_by_ip(ip):
+    try:
+        hostname, _, _ = socket.gethostbyaddr(ip)
+        return hostname
+    except socket.herror:
+        return "Nie znaleziono hosta dla podanego adresu ip"
+    
+ip = sys.argv[1]
+hostname = get_hostname_by_ip(ip)
+print(f'Hostname dla adresu {ip}, to {hostname}')
+
+
+
