69f16905f5770391bafe466b11b276e3e61a853c
Ezdies <bvbclubbvb@gmail.com>
2024-03-27 11:41:16 +0100

    Added ex1 with corresponding client

---
+++ Lab4/clients/ex1client.py
@@ -0,0 +1,30 @@
+import socket
+from time import gmtime, strftime
+
+def send_receive_message(sock, message):
+    # Send the message to the server
+    sock.sendall(message.encode('utf-8'))
+
+    # Receive the response
+    response = sock.recv(1024).decode()
+
+    return response
+
+if __name__== '__main__':
+    sockIPv4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockIPv4.settimeout(1)
+    
+    try:
+        sockIPv4.connect(('0.0.0.0', 2900))
+        print('Connected')
+        
+        # Send a message to the server
+        message_to_send = "Hello, server!"
+        response = send_receive_message(sockIPv4, message_to_send)
+        
+        print("Received response:", response)
+        
+    except socket.error as e:
+        print(f"Failed to connect: {e}")
+    finally:
+        sockIPv4.close()
